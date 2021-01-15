cdaffeb036c9a442562fc4d969bd2f7d9f1d4489
Jeremy Dombrowski <jeremy@meatflavour.dev>
2021-01-15 01:28:15 -0800

    Admin Basic Auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,15 @@
 class Admin::DashboardController < ApplicationController
+  before_filter :authentication_check
+
   def show
+    puts "This string"
   end
+
+   private
+    def authentication_check
+     authenticate_or_request_with_http_basic do |user, password|
+      user == ENV["ADMIN_USER"] && password == ENV["ADMIN_PASS"]
+     end
+    end
+
 end
